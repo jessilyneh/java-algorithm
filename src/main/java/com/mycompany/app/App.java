ecb9d9f5ad74ec2b15c152c35fa61cff997ca4fc
Jessica Felix <37668064+jessilyneh@users.noreply.github.com>
2024-02-22 01:54:12 +0000

    adiciona mais operações com arrays

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,15 +8,33 @@
  import java.util.Arrays;
  
  public class App {
- 
+    static String[] planets = {"Mercurio", "Vênus", "Terra", "Marte", "Júpiter"};
      public static void main(String[] args) {
-        String[] planets = {"Mercurio", "Vênus", "Terra", "Marte", "Júpiter"};
         System.out.println("Planetas: " + Arrays.toString(planets));
- 
+
+
         for (int i = planets.length - 1; i > 0; i--) {
             planets[1] = planets[i - 1];
         }
-         
         System.out.println("Planetas após a modificação: " + Arrays.toString(planets));
+
+        // adiciona um novo valor
+        planets[1] = "VENUS";
+        System.out.println("Adiciona novo elemento:" + Arrays.toString(planets));
+
+        // Insere um elemento em uma posição arbitrária
+        int position = 3;
+        for (int i = planets.length-1; i > position; i--) {
+            planets[i] = planets[i-1];
+            planets[position] = "MARTE";
+            System.out.println("Adiciona novo elemento na posição "  + position + ":" + Arrays.toString(planets));
+        }
+
+        // apaga um elemento em uma posição arbitrária
+        int newPosition = 2;
+        for (int i = newPosition; i < planets.length -1 ; ++i) {
+            planets[i] = planets[i + 1];
+            System.out.println("remove o elemento "  + newPosition + ":" + Arrays.toString(planets));
+        }
      }
  }
